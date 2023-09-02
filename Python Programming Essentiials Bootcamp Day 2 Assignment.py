0db7d191db31884ae002a028b79cc3bd896ee616
ProSouvik <143882078+ProSouvik@users.noreply.github.com>
2023-09-02 17:40:26 +0530

    Day 2 Assignment Submitted

---
+++ Python Programming Essentiials Bootcamp Day 2 Assignment.py
@@ -0,0 +1,19 @@
+#Hello LetsUpgrade Community You Guys Are Awesome
+
+#Taking Two Inputs From User In Variable a & b
+a = int(input("Assign A Number To 'a': "))
+b = int(input("Assign A Number To 'b': "))
+
+#Code To Get Floor Division i.e. a//b
+print("Floor Division Between",a,"and",b,"Is:",a//b)
+
+#Code To Get a Power b i.e. a**b
+print(a,"To The Power Of",b,"Is:",a**b)
+
+#Code To Find Out If a & b Are Equal Or Not
+if a==b:
+    print("True")
+else:
+    print("False")
+
+#Assignment Submitted In Github
