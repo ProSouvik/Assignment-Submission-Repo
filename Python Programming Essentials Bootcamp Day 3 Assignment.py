bf6a1b7613156a704b5224207b168a2fe7b3cab5
ProSouvik <143882078+ProSouvik@users.noreply.github.com>
2023-09-03 17:38:58 +0530

    Day 3 Assignment Submitted

---
+++ Python Programming Essentials Bootcamp Day 3 Assignment.py
@@ -0,0 +1,15 @@
+# Hello LetsUpgrade Community You Guys Are Awesome
+
+#Program Taking A Input From User As A Side Of The Square And Calculating The Area Of It Using area() func.
+
+#Defining The Function
+print("===========================WELCOME========================================================")
+def area (a):
+    area = a**2  # Defining The Function
+    print("The Area Of Your Square Is:",area,"Sq. Units")
+a = int(input("Please Enter The Value Of A Side Of Your Square: "))
+area(a) # Calling The Function
+
+print("===========================THANK YOU======================================================")
+
+#Assignment Submitted In Github Repo
